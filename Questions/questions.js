60802b21ab72e1a66dfa591be0d3309f210d575a
Francisco Thiago <75057408+Francisco-Thiago@users.noreply.github.com>
2022-06-03 05:47:57 -0300

    Add files via upload

--- Questions/questions.js
+++ Questions/questions.js
@@ -1,3 +1,4 @@
+var userRandomGenerated = [];
 async function getRandomUser() {
 
     function getWeight(max, min){
@@ -8,24 +9,30 @@ async function getRandomUser() {
         return Number("1." + Math.floor(Math.random() * (max - min) + min));
     }
 
-    const url = "https://randomuser.me/api/";
+    const url = "https://randomuser.me/api?results=250";
 
     return axios.get(url)
     .then(response => {
-        const data = response['data']['results'][0];
-        const gender = data.gender.charAt(0).toUpperCase();
-        const name = `${(data.name).first} ${(data.name).last}` 
-        const height = getHeight(600, 999);
-        const weight = getWeight(60, 90);
-        const person = {
-            name: name,
-            gender: gender,
-            height: height,
-            weight: weight
+        const data = response['data']['results'];
+        const persons = [];
+        for(let i = 0; i<250 ; i++) {
+            const gender = data[i].gender.charAt(0).toUpperCase();
+            const name = `${(data[i].name).first} ${(data[i].name).last}` 
+            const height = getHeight(600, 999);
+            const weight = getWeight(60, 90);
+            const person = {
+                name: name,
+                gender: gender,
+                height: height,
+                weight: weight
+            }
+            persons.push(person)
         }
-        return person;
+        return persons;
     })
+    .then(person => userRandomGenerated = person)
     .catch(error => console.error(error));
+
 }
 
 function question1(name, workedHours, moneyPerHour, sons) {
@@ -95,15 +102,49 @@ function question3(valueToBePaid, paid) {
     }else {
         throw `O valor pago é insuficiente: ${change}`;
     }    
-    //console.table(exchaged);
 }
 
 function question4() {
-    // Grupo é um suposto array 250 pessoas;
-    const group = [];
-    while(group.length < 5) {
-        group.push(getRandomUser());
-    }
+    let male, female, malePerCent, femalePerCent;
+    const heights = [], weights = [];
+    getRandomUser()
+    .then(() => {
+        let gender;
+
+        gender = userRandomGenerated.filter(value => {
+            return value['gender'] === "M";
+        })
+
+        userRandomGenerated.map(value => {
+            heights.push(value['height']);
+            weights.push(value['weight']);
+        })
+
+        const greaterWeight = weights.reduce((pv, cv) => {
+            return Math.max(pv, cv)
+        })
+
+        const indexOfTheGreaterWeight = weights.indexOf(greaterWeight);
+
+        const totalOfHeights = +(heights.reduce((pv, cv) => {
+            return pv + cv;
+        }, 0)/ heights.length).toFixed(4);
+
+        const result = {
+            tallestPerson: userRandomGenerated[indexOfTheGreaterWeight].name,
+            totalOfHeights: totalOfHeights, 
+            male: gender.length,
+            female: 250 - gender.length,
+            malePerCent: (gender.length * 100) / 250,
+            femalePerCent: ((250 - gender.length) * 100) / 250
+        }
+
+        return result;
+    })
+    .then( result => {
+        console.log("Questão 4"); 
+        console.table(result) 
+    })
 }
 
 function question5(number) {
@@ -117,15 +158,37 @@ function question5(number) {
     }
 
     if((perfectNumbers.reduce((pv, cv) => pv+cv)) === number) {
-        console.table(perfectNumbers.slice(0, 4))
+        console.table(perfectNumbers.slice(0, 4));
     }else {
-        console.log("Número imperfeito")
+        console.log("Número imperfeito");
     }
 }
 
+function question6(I, A, B, C) {
+    console.log("Questão 6");
+    let numbers = [A, B, C];
+    numbers = numbers.sort((a, b) => {
+        return a < b ? -1 : a > b ? 1 : 0;
+    })
+
+    if(I == 1) {
+        console.log("Números ordenados: " + numbers.join(", "))
+    }else if(I == 2) {
+        console.log("Números invertidos: " + numbers.reverse().join(", "))
+    }else if(I == 3) {
+        let num = numbers[1] 
+        numbers[1] = numbers[2]
+        numbers[2] = num; 
+        console.log("Números posicionados: " + numbers.join(", "))
+    }else {
+        console.log("I - Inválido.")
+    }
+}
 
 
 question1("Martin", 10, 20, 5);
 question2("Haroldo", 100, 560, 6);
 question3(200, 499);
+question4()
 question5(28)
+question6(4, 10, 20, 30)
